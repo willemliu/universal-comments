f4e3499cb89d5f9188ecd1fd03eb4bc0294454b1
Willem Liu <willem_liu@hotmail.com>
2020-01-20 12:53:47 +0100

    script-src

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,7 +16,7 @@ export default function MyApp({ Component, pageProps }: any) {
                 />
                 <meta
                     httpEquiv="Content-Security-Policy"
-                    content="default-src *; img-src * data:; style-src * 'unsafe-inline'; script-src 'self' https://connect.facebook.net/en_US/sdk.js https://apis.google.com/js/platform.js 'unsafe-inline' 'unsafe-eval';"
+                    content="default-src *; img-src * data:; style-src * 'unsafe-inline'; script-src * 'unsafe-inline' 'unsafe-eval';"
                 />
                 <meta name="uc:disabled" content="true" />
                 <FBSDK key="facebook" />
