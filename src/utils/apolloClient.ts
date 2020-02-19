f9b7d412fa3cbc08bfa787485bc75484ee41efe7
Willem Liu <willem_liu@hotmail.com>
2020-02-19 10:30:34 +0100

    LatestPositiveCircleComments

--- src/utils/apolloClient.ts
+++ src/utils/apolloClient.ts
@@ -898,7 +898,9 @@ export async function getLatestPositiveCircleComments(
                             circle_id: { _eq: $circle_id }
                             removed: { _eq: false }
                             circle: {
-                                users_circles: { user_uuid: { _eq: $uuid } }
+                                users_circles: {
+                                    user: { uuid: { _eq: $uuid } }
+                                }
                             }
                         }
                     ) {
