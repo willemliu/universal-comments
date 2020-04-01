7c7f04fa713145340f9e0c6ea63472748b8c568d
Willem Liu <willem_liu@hotmail.com>
2020-04-01 11:42:09 +0200

    WIP mail

--- pages/api/mail.ts
+++ pages/api/mail.ts
@@ -8,37 +8,32 @@ async function mail(req: any, res: any) {
     const API_KEY = process.env.MAILJET_API_KEY;
     const SECRET_KEY = process.env.MAILJET_SECRET_KEY;
     const mailjet = Mailjet.connect(API_KEY, SECRET_KEY);
-    let mailResult;
-    if (req.query.email && req.query.name) {
-        const request = mailjet.post('send', { version: 'v3.1' }).request({
-            Messages: [
-                {
-                    From: {
-                        Email: 'noreply@willim.nl',
-                        Name: 'Universal Comments',
-                    },
-                    To: [
-                        {
-                            Email: req.query.email,
-                            Name: req.query.name,
-                        },
-                    ],
-                    Subject: 'Testing 123',
-                    TextPart: `Well this is nice isn't it?`,
-                    HTMLPart: `<h3>Well this is nice isn't it?</h3>`,
-                },
-            ],
-        });
-        request
-            .then((result) => {
-                mailResult = result;
-            })
-            .catch((err) => {
-                console.log(err.statusCode);
-            });
-    }
 
     try {
+        let mailResult;
+        if (req.query.email && req.query.name) {
+            const request = mailjet.post('send', { version: 'v3.1' }).request({
+                Messages: [
+                    {
+                        From: {
+                            Email: 'noreply@willim.nl',
+                            Name: 'Universal Comments',
+                        },
+                        To: [
+                            {
+                                Email: req.query.email,
+                                Name: req.query.name,
+                            },
+                        ],
+                        Subject: 'Testing 123',
+                        TextPart: `Well this is nice isn't it?`,
+                        HTMLPart: `<h3>Well this is nice isn't it?</h3>`,
+                    },
+                ],
+            });
+
+            mailResult = request.then((result) => result);
+        }
         res.status(200).json({ status: 'OK', API_KEY, SECRET_KEY, mailResult });
     } catch (e) {
         console.error(e);
