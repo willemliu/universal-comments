2f5461e96818824b7159ebc9fb37eb22330a4063
Willem Liu <willem_liu@hotmail.com>
2020-04-01 11:15:31 +0200

    Changed env vars

--- next.config.js
+++ next.config.js
@@ -9,11 +9,11 @@ module.exports = {
             GRAPHQL_ENDPOINT: process.env.GRAPHQL_ENDPOINT,
             HASURA_ROLE: process.env.HASURA_ROLE,
             HASURA_ADMIN_SECRET: process.env.HASURA_ADMIN_SECRET,
+            MAILJET_API_KEY: process.env.MAILJET_API_KEY,
+            MAILJET_SECRET_KEY: process.env.MAILJET_SECRET_KEY,
         },
     },
     env: {
         // Will only be available on the server side
-        MAILJET_API_KEY: process.env.MAILJET_API_KEY,
-        MAILJET_SECRET_KEY: process.env.MAILJET_SECRET_KEY,
     },
 };
