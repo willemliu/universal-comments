700f7fdf307b2aee9f36aeda2bb76fbde0c3d943
Willem Liu <willem_liu@hotmail.com>
2020-04-01 15:45:58 +0200

    Multiple messages at once

--- pages/api/mail.ts
+++ pages/api/mail.ts
@@ -9,8 +9,7 @@ async function mail(req: any, res: any) {
     const API_KEY = process.env.MAILJET_API_KEY;
     const SECRET_KEY = process.env.MAILJET_SECRET_KEY;
     const mailjet = Mailjet.connect(API_KEY, SECRET_KEY);
-    const mailPromises = [];
-    const mailResults = [];
+    const messages = [];
     let otherUsers;
 
     try {
@@ -28,46 +27,43 @@ async function mail(req: any, res: any) {
                     otherUsers?.comments?.length
                 );
                 if (otherUsers?.comments?.length) {
-                    const request = mailjet
-                        .post('send', { version: 'v3.1' })
-                        .request({
-                            Messages: [
-                                {
-                                    From: {
-                                        Email: 'noreply@willim.nl',
-                                        Name: 'Universal Comments',
-                                    },
-                                    To: [
-                                        {
-                                            Email: user.email,
-                                            Name: user.display_name,
-                                        },
-                                    ],
-                                    Subject: `New comment: ${url}`,
-                                    TextPart: `
+                    messages.push({
+                        From: {
+                            Email: 'noreply@willim.nl',
+                            Name: 'Universal Comments',
+                        },
+                        To: [
+                            {
+                                Email: user.email,
+                                Name: user.display_name,
+                            },
+                        ],
+                        Subject: `New comment: ${url}`,
+                        TextPart: `
 A new comment has been has been posted here: ${url}.
 
 "${otherUsers?.comments?.[0]?.comment}"
 
 You're receiving this e-mail because you've left a comment at this url before.`,
-                                    HTMLPart: `
+                        HTMLPart: `
 <h3>A new comment</h3>
 <p>A new comment has been has been posted here: <a href="${url}" target="_blank" rel="noopener noreferrer">${url}</a>.</p>
 <h2>"${otherUsers?.comments?.[0]?.comment}"</h2>
 <small>You're receiving this e-mail because you've left a comment at this url before.</small>
-                `,
-                                },
-                            ],
-                        });
-                    mailPromises.push(request);
+    `,
+                    });
                 }
             });
         }
-        if (mailPromises.length) {
-            mailResults.push(...(await Promise.all(mailPromises)));
-            console.log(mailResults);
+
+        if (messages.length) {
+            const request = mailjet.post('send', { version: 'v3.1' }).request({
+                Messages: messages,
+            });
+            await request.then(console.log);
         }
-        res.status(200).json({ status: 'OK', mailResults: mailResults.length });
+
+        res.status(200).json({ status: 'OK', messages });
     } catch (e) {
         console.error(e);
         res.status(504).json({ error: e });
