62725d00531e8bc5829f3dd033cdd728fad82514
Willem Liu <willem_liu@hotmail.com>
2020-04-01 11:52:06 +0200

    env

--- next.config.js
+++ next.config.js
@@ -14,6 +14,12 @@ module.exports = {
         },
     },
     env: {
-        // Will only be available on the server side
+        AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+        AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+        GRAPHQL_ENDPOINT: process.env.GRAPHQL_ENDPOINT,
+        HASURA_ROLE: process.env.HASURA_ROLE,
+        HASURA_ADMIN_SECRET: process.env.HASURA_ADMIN_SECRET,
+        MAILJET_API_KEY: process.env.MAILJET_API_KEY,
+        MAILJET_SECRET_KEY: process.env.MAILJET_SECRET_KEY,
     },
 };
