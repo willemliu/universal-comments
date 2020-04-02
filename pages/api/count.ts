aaaaaa2fe8f50d86f6e712f211c89ba4c8562a8c
Willem Liu <willem_liu@hotmail.com>
2020-04-02 11:39:40 +0200

    Removed cache control header

--- pages/api/count.ts
+++ pages/api/count.ts
@@ -3,11 +3,6 @@ import { getCommentCount } from '../../src/utils/apolloClient';
 const cors = Cors({ allowMethods: ['GET', 'HEAD'] });
 
 async function count(request: any, response: any) {
-    response.setHeader(
-        'Cache-Control',
-        'max-age=0, s-maxage=1, stale-while-revalidate'
-    );
-
     const url = request.query.canonical;
 
     try {
